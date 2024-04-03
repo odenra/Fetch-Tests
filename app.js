6e27375f2a632446f59fa9b0816598da1dcbd4e1
odenra <ilkerkaraoden@gmail.com>
2024-04-03 14:05:31 +0300

    rest api test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.querySelector("#button1").addEventListener("click", getTextFile);
 document.querySelector("#button2").addEventListener("click", getJsonFile);
+document.querySelector("#button3").addEventListener("click", getAPI);
 
 
 function getTextFile(){
@@ -21,6 +22,25 @@ function getJsonFile(){
         articles.forEach(function(article){
 
             output += `<li>Başlık${article.title} - İçerik${article.body}</li>`
+        })
+
+        output+= "</ul>"
+
+        document.getElementById("output").innerHTML += output;
+    }).catch(function(err){
+        console.log(err)
+    });
+};
+
+function getAPI(){
+    fetch("https://api.github.com/users").then(function(response){
+        return response.json();
+    }).then(function(users){
+        let output = "<ul>";
+
+        users.forEach(function(user){
+
+            output += `<li>Kullanıcı Adı${user.login}</li>`
         })
 
         output+= "</ul>"
